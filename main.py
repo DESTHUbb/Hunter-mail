b34428829142ff186937fc4b3317021a74eff204
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-04-27 02:35:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ api_key = config.API_KEY
 def format_phone_number(phone_number):
     """Removes non-numeric characters from a phone number."""
     
+    
+       characters_to_remove = {'-', '.', '(', ')'}
+    return ''.join(char for char in phone_number if char not in characters_to_remove)
+
